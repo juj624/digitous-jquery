d0c20d46b06c988e8d69f6a1c63144da6b3ca83b
juj624 <julienpenot@outlook.com>
2021-07-05 17:32:54 +0200

    exercice-32

---
+++ exercice32/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $(".btn-success").click(() => {
+
+        $.ajax({
+            url: 'https://restcountries.eu/rest/v2/name/france',
+            success: function (data, statuts, response) {
+
+                console.log(data[0].name);
+                console.log(data[0].capital);
+            }
+        });
+    });
+
+});
+
